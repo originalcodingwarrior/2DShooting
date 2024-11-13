544cde2ede1bf6004862063ce695a41c2b1747be
DDD <dayeon3721@gmail.com>
2024-11-13 19:10:26 +0900

    또미지 넣기
    또 이미지 넣음

--- Assets/Scripts/AngerBar.cs
+++ Assets/Scripts/AngerBar.cs
@@ -9,21 +9,18 @@ public class AngerBar : MonoBehaviour
 
     public Slider slider;
     public Image fillImage;
-    public Gradient gradient;
 
     // Start is called before the first frame update
 
     public void SetValue(int value)
     {
         slider.value = value;
-        fillImage.color = gradient.Evaluate(slider.normalizedValue);
     }
 
     void Start()
     {
-        slider.maxValue = 20;
+        slider.maxValue = 10;
         slider.value = 0;
-        fillImage.color = gradient.Evaluate(0f);
     }
 
     // Update is called once per frame
